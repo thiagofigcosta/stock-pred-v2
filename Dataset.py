9cb589f6415bf5cc029cb9ffcde2f3ef8691a50c
Computa Hacker Boy <thiagofigcosta@hotmail.com>
2021-06-17 20:27:22 -0300

    improving the new dataset class

--- Dataset.py
+++ Dataset.py
@@ -5,195 +5,60 @@ from Utils import Utils
 
 class Dataset:
 	class Entry:
-		def __init__(self,index=None,date_index=None,value=None,backward_values=[],forward_values=None):
+		def __init__(self,index=None,date_index=None,stock_value=None,features=None,backward_values=[],forward_values=None,has_only_indexes=False):
 			self.index=index # of position 0 backward value
 			self.date_index=date_index # of position 0 backward value
-			if value is not None:
-				backward_values=[tuple([value])]
+			self.has_only_indexes=has_only_indexes
+			if stock_value is not None:
+				backward_values=[[stock_value]]
 				forward_values=None
-			self.backward_values=backward_values # array of tuple of values
-			self.forward_values=forward_values # array of tuple of values
+				if features is not None:
+					backward_values[0]+=features
+			self.backward_values=backward_values # 2d array of values
+			self.forward_values=forward_values # 2d array of values
 			self.next=None # next entry
 
-		def mergeWith(self,dataset):
-			i=0
-			size_a=self.getSize(consider_null_forwards=True)
-			size_b=dataset.getSize(consider_null_forwards=True)
-			cur_a=self
-			cur_b=dataset
-			while i<size_a and i<size_b:
-				if cur_a.backward_values is None:
-					cur_a.backward_values=cur_b.backward_values
-				elif cur_b.backward_values is None:
-					cur_a.backward_values=cur_a.backward_values
-				else:
-					largest=cur_a.backward_values
-					if len(cur_a.backward_values)<len(cur_b.backward_values):
-						largest=cur_b.backward_values
-					for j in range(min(len(cur_a.backward_values),len(cur_b.backward_values))):
-						largest[j]=cur_a.backward_values[j]+cur_b.backward_values[j]
-					cur_a.backward_values=largest
-
-				if cur_a.forward_values is None:
-					cur_a.forward_values=cur_b.forward_values
-				elif cur_b.forward_values is None:
-					cur_a.forward_values=cur_a.forward_values
-				else:
-					largest=cur_a.forward_values
-					if len(cur_a.forward_values)<len(cur_b.forward_values):
-						largest=cur_b.forward_values
-					for j in range(min(len(cur_a.forward_values),len(cur_b.forward_values))):
-						largest[j]=cur_a.forward_values[j]+cur_b.forward_values[j]
-					cur_a.forward_values=largest
-				i+=1
-				if cur_a.next is None or cur_b.next  is None:
+		def regenerateIndexes(self):
+			index=0
+			cur=self
+			while True:
+				if not cur.has_only_indexes:
+					cur.index=index
+					index+=1
+				if cur.next is None:
 					break
-				cur_a=cur_a.next
-				cur_b=cur_b.next
-
-		def getNeuralNetworkArray(self):
-			pass # TODO return X and Y arrays for LSTM network
-
-			# como é hoje:
-				# X_train (3418, 20, 1)
-				# Y_train (3418, 7)
-				# X_val (855, 20, 1)
-				# Y_val (855, 7)
-				# X_test (1069, 20, 1)
-				# Y_test (1062, 7)
-
-
-			# como vou fazer:
-				# X_train (3418, 20, 1, 1) # (amostras, for/backwards, empresa, features)
-				# Y_train (3418, 7, 1) # (amostras, for/backwards, empresa)
-				# X_val (855, 20, 1, 1) # (amostras, for/backwards, empresa, features)
-				# Y_val (855, 7, 1) # (amostras, for/backwards, empresa)
-				# X_test (1069, 20, 1, 1)# (amostras, for/backwards, empresa, features)
-				# Y_test (1062, 7, 1) # (amostras, for/backwards, empresa)
-
-		@staticmethod
-		def datasetFromNeuralNetworkArray(X,Y):
-			pass # TODO
-
-		def generateBackAndForward(self,back_samples,forward_samples):
-			values=self.getValueArray()
-			size=self.getSize(consider_null_forwards=True)
-			new_size=size-back_samples+1
-			new_useful_size=size-back_samples+1-forward_samples
-			new_full_size=new_size+forward_samples
-			last=self.getNthPointer(size-1)
-			if last.date_index is not None:
-				next_dates=Utils.getStrNextNWorkDays(last.date_index,forward_samples)
-			else:
-				next_dates=[None]*forward_samples
-			dates=self.getDateArray(consider_null_forwards=True)
-			new_first=None
-			cur=new_first
-			for i in range(back_samples-1):
-				new_el=Dataset.Entry(index=i,date_index=dates[i],backward_values=[values[i]])
-				if new_first is None:
-					new_first=new_el
-					cur=new_first
 				else:
-					cur.next=new_el
 					cur=cur.next
-			dates=dates[back_samples-1:]+next_dates
-			starting_at=self.getNthPointer(back_samples-1).index
-			if starting_at is None:
-				starting_at=0
-			indexes=list(range(starting_at,new_full_size+starting_at,1))
-			for i in range(new_full_size):
-				if i<new_size:
-					back_values=[]
-					for j in range(back_samples):
-						back_values.append(values[back_samples+i-j-1]+tuple()) # copy of tuple
-					for_values=[]
-					if i<new_useful_size:
-						for j in range(forward_samples):
-							for_values.append(values[back_samples+i+j]+tuple()) # copy of tuple
-					new_el=Dataset.Entry(index=indexes[i],date_index=dates[i],backward_values=back_values,forward_values=for_values)
-				else:
-					new_el=Dataset.Entry(index=indexes[i],date_index=dates[i],backward_values=None,forward_values=None)
 
-				if new_first is None:
-					new_first=new_el
-					cur=new_first
-				else:
-					cur.next=new_el
-					cur=cur.next
-			return new_first
-
-		def getValueArray(self,degree=0):
-			val_arr=[]
-			cur=self
-			while True:
-				if cur.backward_values is None:
-					break
-				if degree>0:
-					if cur.forward_values is not None:
-						val_arr.append(cur.forward_values[abs(degree-1)])
-					else:
-						break
-				else:
-					val_arr.append(cur.backward_values[abs(degree)])
-				if cur.next is None:
-					break
-				cur=cur.next
-			return val_arr
-
-		def getDateArray(self,degree=0,consider_null_forwards=False):
-			date_arr=[]
-			cur=self
-			extra=0
-			while True:
-				date_arr.append(cur.date_index)
-				if cur.next is None:
-					break
-				cur=cur.next
-				if cur.forward_values is None and not consider_null_forwards:
-					if degree==0:
-						break
-					else:
-						extra+=1
-			if degree>0:
-				date_arr=date_arr[degree:]
-				if extra>0:
-					to_remove=extra-degree
-					if to_remove>0:
-						date_arr=date_arr[:-to_remove]
-			return date_arr
-
-		def getIndexArray(self,degree=0,consider_null_forwards=True):
-			index_arr=[]
-			cur=self
-			extra=0
-			while True:
-				index_arr.append(cur.index)
-				if cur.next is None:
-					break
-				cur=cur.next
-				if cur.forward_values is None and not consider_null_forwards:
-					if degree==0:
-						break
-					else:
-						extra+=1
-			if degree>0:
-				index_arr=index_arr[degree:]
-				if extra>0:
-					to_remove=extra-degree
-					if to_remove>0:
-						index_arr=index_arr[:-to_remove]
-			return index_arr
-
-		def getSize(self,consider_null_forwards=False):
+		def getSize(self,consider_indexes=False):
 			size=0
 			cur=self
 			while True:
-				size+=1
-				if cur.next is None or (cur.forward_values is None and not consider_null_forwards):
+				if not cur.has_only_indexes or consider_indexes:
+					size+=1
+				if cur.next is None:
 					break
 				cur=cur.next
 			return size
+
+		def printRawValues(self):
+			i=0
+			size=self.getSize(consider_indexes=True)
+			cur=self
+			while i<size:
+				to_print=''
+				if cur.date_index is not None:
+					to_print+=str(cur.date_index)+': '
+				elif cur.index is not None:
+					to_print+=str(cur.index)+': '
+				to_print+=str(cur.backward_values)
+				if cur.forward_values:
+					to_print+='->'+str(cur.forward_values)
+				print(to_print)
+				i+=1
+				if cur.next is None:
+					break
+				cur=cur.next
 
 		def getNthPointer(self,n):
 			i=0
@@ -206,61 +71,214 @@ class Dataset:
 			return cur
 
 		def getPointerAtIndex(self,index):
-			i=0
-			size=self.getSize()
 			cur=self
-			while i<size:
+			while True:
 				if cur.index==index:
 					return cur
-				i+=1
 				if cur.next is None:
 					break
 				cur=cur.next
 			return None
 
 		def getPointerAtDate(self,date):
-			i=0
-			size=self.getSize()
 			cur=self
-			while i<size:
+			while True:
 				if cur.date_index==date:
 					return cur
-				i+=1
 				if cur.next is None:
 					break
 				cur=cur.next
 			return None
 
-		def regenerateIndexes(self):
-			index=0
+		def getValues(self,degree=0):
+			val_arr=[]
 			cur=self
 			while True:
-				cur.index=index
-				index+=1
+				if not cur.has_only_indexes:
+					if degree>0:
+						if cur.forward_values is not None:
+							val_arr.append(cur.forward_values[abs(degree-1)])
+						else:
+							break
+					else:
+						val_arr.append(cur.backward_values[abs(degree)])
 				if cur.next is None:
 					break
 				cur=cur.next
+			return val_arr
 
-		def printDatesAndValues(self,degree=0,consider_null_forwards=False):
-			dates=self.getDateArray(degree=degree,consider_null_forwards=True)
-			values=self.getValueArray(degree=degree)
-			size=self.getSize(consider_null_forwards=consider_null_forwards)
-
-			for i in range(size):
-				print('{}-{}'.format(dates[i],values[i]))
-
-		def printRawDatesAndValues(self):
-			i=0
-			size=self.getSize(consider_null_forwards=True)
+		def getIndexes(self,degree=0,int_index=False,get_all=False):
+			idx_arr=[]
 			cur=self
-			while i<size:
-				print('{}: {}->{}'.format(cur.date_index,cur.backward_values,cur.forward_values))
-				i+=1
+			extra=0
+			first_value_found=False
+			while True:
+				if int_index:
+					idx_arr.append(cur.index)
+				else:
+					idx_arr.append(cur.date_index)
 				if cur.next is None:
 					break
 				cur=cur.next
+				if cur.has_only_indexes:
+					extra+=1
+				else:
+					first_value_found=True
+			if degree>0 and not get_all:
+				idx_arr=idx_arr[degree:]
+			if extra>0 and not get_all:
+				to_remove=extra-degree
+				if to_remove>0:
+					idx_arr=idx_arr[:-to_remove]
+			return idx_arr
+			
+	def convertToTemporalValues(self,back_samples,forward_samples):
+		if self.converted:
+			raise Exception('Already converted')
+		self.converted=not self.converted
+		values=self.getValues()
+		size=self.getSize()
+		new_size=size-back_samples+1
+		new_useful_size=size-back_samples+1-forward_samples
+		new_full_size=new_size+forward_samples
+		last=self.data.getNthPointer(size-1)
+		if last.date_index is not None:
+			next_dates=Utils.getStrNextNWorkDays(last.date_index,forward_samples)
+		else:
+			next_dates=[None]*forward_samples
+		dates=self.getIndexes()
+		new_first=None
+		cur=new_first
+		for i in range(back_samples-1):
+			new_el=Dataset.Entry(index=i,date_index=dates[i],backward_values=[values[i]])
+			if new_first is None:
+				new_first=new_el
+				cur=new_first
+			else:
+				cur.next=new_el
+				cur=cur.next
+		dates=dates[back_samples-1:]+next_dates
+		starting_at=self.data.getNthPointer(back_samples-1).index
+		if starting_at is None:
+			starting_at=0
+		indexes=list(range(starting_at,new_full_size+starting_at,1))
+		for i in range(new_full_size):
+			if i<new_size:
+				back_values=[]
+				for j in range(back_samples):
+					back_values.append(values[back_samples+i-j-1].copy())
+				for_values=[]
+				if i<new_useful_size:
+					for j in range(forward_samples):
+						for_values.append(values[back_samples+i+j].copy())
+				new_el=Dataset.Entry(index=indexes[i],date_index=dates[i],backward_values=back_values,forward_values=for_values)
+			else:
+				new_el=Dataset.Entry(index=indexes[i],date_index=dates[i],backward_values=None,forward_values=None,has_only_indexes=True)
+
+			if new_first is None:
+				new_first=new_el
+				cur=new_first
+			else:
+				cur.next=new_el
+				cur=cur.next
+		self.data=new_first
+		self.converted_params=(back_samples,forward_samples)
+		self.regenerateIndexes()
+
+	def revertFromTemporalValues(self):
+		if not self.converted:
+			raise Exception('Not converted yet')
+		self.converted=not self.converted
+		back_samples,forward_samples=self.converted_params
+		self.converted_params=tuple()
+
+		values=self.getValues()
+		dates=self.getIndexes(get_all=True)
+		raise Exception('TODO code me')
+		self.regenerateIndexes()
+
+	def getNeuralNetworkArrays(self):
+		# (amostras, for/backwards, empresa, features)
+		cur=self
+		while True:
+			if cur.index==index:
+				return cur
+			if cur.next is None:
+				break
+			cur=cur.next
+		return None
 	
-	def __init__(self,name,data,scaler):
+	def addCompany(self,stock_value_array,date_array=None,features_2d_array=None):
+		if self.converted:
+			raise Exception('Already converted')
+		date_array_len=float('inf')
+		if date_array is not None:
+			date_array_len=len(date_array)
+		stock_value_array_len=float('inf')
+		if stock_value_array is not None:
+			stock_value_array_len=len(stock_value_array)
+		length=min(stock_value_array_len,date_array_len)
+		cur=self.data
+		first_company=cur is None
+		for i in range(length):
+			# get data
+			cur_date=None
+			if date_array is not None:
+				cur_date=date_array[i]
+			cur_stock=stock_value_array[i]
+			cur_features=None
+			if features_2d_array is not None:
+				cur_features=features_2d_array[i]
+				if type(cur_features) is not list:
+					if type(cur_features) is tuple:
+						cur_features=list(cur_features)
+					else:
+						cur_features=[cur_features]
+
+			# add data to linked list
+			if first_company:
+				entry=Dataset.Entry(index=i,stock_value=cur_stock,date_index=cur_date,features=cur_features)
+				if cur is None:
+					cur=entry
+				else:
+					cur.next=entry
+					cur=cur.next
+				if self.data is None:
+					self.data=entry
+			else:
+				if not cur.has_only_indexes:
+					new_company_values=[cur_stock]
+					if cur_features is not None:
+						new_company_values[0]+=cur_features
+					if cur_date is None or cur_date==cur.date_index:
+						cur.backward_values.append(new_company_values)
+				if cur.next is None:
+					break
+				else:
+					cur=cur.next
+		if first_company:
+			self.data.regenerateIndexes()
+
+	def printRawValues(self):
+		print('Name: '+self.name)
+		print('Converted:',self.converted)
+		print('Data: ')
+		self.data.printRawValues()
+
+	def getSize(self,consider_indexes=False):
+		return self.data.getSize(consider_indexes=consider_indexes)
+
+	def getValues(self,degree=0):
+		return self.data.getValues(degree=degree)
+
+	def getIndexes(self,degree=0,int_index=False,get_all=False):
+		return self.data.getIndexes(degree=degree,int_index=int_index,get_all=get_all)
+
+	def regenerateIndexes(self):
+		self.data.regenerateIndexes()
+	
+	def __init__(self,name):
 		self.name=name
-		self.data=data
-		self.scaler=scaler
+		self.data=None
+		self.converted=False
+		self.converted_params=tuple()
--- test.py
+++ test.py
@@ -10,71 +10,76 @@ from Utils import Utils
 from Dataset import Dataset
 
 
-a=Dataset.Entry()
-a.backward_values=[tuple([0])]
-a.forward_values=[tuple([1])]
-
-a.next=Dataset.Entry()
-a.next.backward_values=[tuple([2])]
-a.next.forward_values=[tuple([3])]
-
-a.next.next=Dataset.Entry()
-a.next.next.backward_values=[tuple([4])]
-a.next.next.forward_values=[tuple([5])]
-
-a.next.next.next=Dataset.Entry()
-a.next.next.next.backward_values=[tuple([4])]
-a.next.next.next.forward_values=None
-
-a.regenerateIndexes()
-print('size',a.getSize())
-print('idx',a.getIndexArray(0))
-print('idx-1',a.getIndexArray(1))
-print('date',a.getDateArray(0))
-print('val',a.getValueArray(0))
-print('val-future',a.getValueArray(1))
-print('index of nth',a.getNthPointer(2).index)
-print('index of atIdx',a.getPointerAtIndex(2).index)
-print('index of last',a.getPointerAtIndex(a.getSize()-1).index)
-print('Next 6', Utils.getStrNextNWorkDays('06/06/2021',6))
+stock_value=[1,2,3,4,5,6,7,8,9,10]
+features_values=[[10],[20],[30],[40],[50],[60],[70],[80],[90],100]
+dates=Utils.getStrNextNWorkDays('17/06/2021',len(stock_value))
 
 
-print('------------------------\n')
-a=Dataset.Entry(date_index='06/06/2021',value=1)
-a.next=Dataset.Entry(date_index='07/06/2021',value=2)
-a.next.next=Dataset.Entry(date_index='08/06/2021',value=3)
-a.next.next.next=Dataset.Entry(date_index='09/06/2021',value=4)
-a.next.next.next.next=Dataset.Entry(date_index='10/06/2021',value=5)
-a.next.next.next.next.next=Dataset.Entry(date_index='11/06/2021',value=6)
-a.next.next.next.next.next.next=Dataset.Entry(date_index='14/06/2021',value=7)
-a.next.next.next.next.next.next.next=Dataset.Entry(date_index='15/06/2021',value=8)
-a.regenerateIndexes()
-print('indexes',a.getIndexArray())
+# -------------------
+print('-------------------')
+dataset=Dataset(name='OriginalGE')
+dataset.addCompany(stock_value)
+print('Len:',dataset.getSize())
+dataset.printRawValues()
+# -------------------
+print('-------------------')
+dataset=Dataset(name='OriginalGE')
+dataset.addCompany(stock_value,dates)
+print('Len:',dataset.getSize())
+dataset.printRawValues()
+# -------------------
+print('-------------------')
+dataset=Dataset(name='OriginalGE')
+dataset.addCompany(stock_value,dates,features_values)
+print('Len:',dataset.getSize())
+dataset.printRawValues()
+print('Indexes:',dataset.getIndexes())
+print('Values:',dataset.getValues())
+# -------------------
+print('-------------------')
+dataset.convertToTemporalValues(3,2)
+print('Len:',dataset.getSize())
+dataset.printRawValues()
+print('Indexes:',dataset.getIndexes())
+print('Values:',dataset.getValues())
+# -------------------
+print('-------------------')
+stock_value_2=[el+1 for el in stock_value]
+dataset=Dataset(name='OriginalGE and OriginalGE+1')
+dataset.addCompany(stock_value)
+dataset.addCompany(stock_value_2)
+print('Len:',dataset.getSize())
+dataset.printRawValues()
+# -------------------
+print('-------------------')
+dataset.convertToTemporalValues(3,2)
+print('Len:',dataset.getSize())
+dataset.printRawValues()
+print('Indexes:',dataset.getIndexes())
+print('Values:',dataset.getValues())
 
-print('Original:')
-a.printRawDatesAndValues()
 
-print('\nback=4, for=3:')
-b=a.generateBackAndForward(4,3)
-b.printRawDatesAndValues()
-print('indexes',b.getIndexArray())
-print('dates',b.getDateArray(consider_null_forwards=True))
-print('values',b.getValueArray())
 
-print('\nback=4, for=2:')
-c=a.generateBackAndForward(4,2)
-c.printRawDatesAndValues()
-print('indexes',c.getIndexArray())
 
-print('------------------------\n')
-b=Dataset.Entry(date_index='06/06/2021',value='a')
-b.next=Dataset.Entry(date_index='07/06/2021',value='b')
-b.next.next=Dataset.Entry(date_index='08/06/2021',value='c')
-b.next.next.next=Dataset.Entry(date_index='09/06/2021',value='d')
-b.next.next.next.next=Dataset.Entry(date_index='10/06/2021',value='e')
-b.next.next.next.next.next=Dataset.Entry(date_index='11/06/2021',value='f')
-b.next.next.next.next.next.next=Dataset.Entry(date_index='14/06/2021',value='g')
 
-a.mergeWith(b)
 
-a.printRawDatesAndValues()
+
+
+
+
+
+# -------------------
+print('-------------------')
+dataset_x,dataset_y=dataset.getNeuralNetworkArrays()
+print('Neural Network X Shape:',dataset_x.shape)
+print('Neural Network Y Shape:',dataset_y.shape)
+print('Neural Network X:',dataset_x)
+print('Neural Network Y:',dataset_y)
+
+# -------------------
+print('-------------------')
+dataset.revertFromTemporalValues()
+print('Len:',dataset.getSize())
+dataset.printRawValues()
+print('Indexes:',dataset.getIndexes())
+print('Values:',dataset.getValues())
