fix predictions graph, fix normalization, test normalization gap, cleanup

Dataset.py
@@ -85,7 +85,6 @@ class Dataset:
 				if not cur.has_only_indexes:
 					if cur.backward_values is not None:
 						if amount_of_features is None:
-							print(cur.backward_values)
 							amount_of_features=len(cur.backward_values[0][0])
 							max_vals=[0]*amount_of_features
 						for a in cur.backward_values:
@@ -411,8 +410,10 @@ class Dataset:
 		if normalization in (Dataset.Normalization.NORMALIZE,Dataset.Normalization.NORMALIZE_WITH_GAP):
 			maxes=self.getAbsMaxes()
 			if normalization == Dataset.Normalization.NORMALIZE_WITH_GAP:
+				maxes=list(maxes)
 				for i in range(len(maxes)):
 					maxes[i]*=1.1 # gap percentage
+				maxes=tuple(maxes)
 			self.normalization_params=maxes
 			normalize=True
 		if normalization == Dataset.Normalization.NORMALIZE_WITH_EXTERNAL_MAXES:
@@ -465,15 +466,7 @@ class Dataset:
 			raise Exception('Not converted yet')
 		if len(Y.shape)==2:
 			Y=self.reshapeLabelsFromNeuralNetwork(Y)
-		normalize = self.normalization_method is not None and self.normalization_method in (Dataset.Normalization.NORMALIZE,Dataset.Normalization.NORMALIZE_WITH_GAP,Dataset.Normalization.NORMALIZE_WITH_EXTERNAL_MAXES)
-		if normalize and (self.normalization_params is None or len(self.normalization_params)==0):
-			raise Exception('No normalization params found')
-		if normalize:
-			for a,el_1 in enumerate(Y):
-				if type(el_1) in (list,np.ndarray):
-					for b,el_2 in enumerate(el_1):
-						print(el_2)
-						Y[a][b]=el_2*float(self.normalization_params[0])
+		Y=self.denormalizeLabelsFromNeuralNetwork(Y)
 		starting_point=self.data.getPointerAtIndex(start_index)
 		if starting_point is None:
 			return
@@ -485,6 +478,17 @@ class Dataset:
 				break
 			cur=cur.next
 			i+=1
+
+	def denormalizeLabelsFromNeuralNetwork(self,Y):
+		normalize = self.normalization_method is not None and self.normalization_method in (Dataset.Normalization.NORMALIZE,Dataset.Normalization.NORMALIZE_WITH_GAP,Dataset.Normalization.NORMALIZE_WITH_EXTERNAL_MAXES)
+		if normalize and (self.normalization_params is None or len(self.normalization_params)==0):
+			raise Exception('No normalization params found')
+		if normalize:
+			for a,el_1 in enumerate(Y):
+				if type(el_1) in (list,np.ndarray):
+					for b,el_2 in enumerate(el_1):
+						Y[a][b]=el_2*float(self.normalization_params[0])
+		return Y
 
 	def reshapeFeaturesToNeuralNetwork(self,X):
 		in_shape=X.shape

NeuralNetwork.py
@@ -95,7 +95,8 @@ class NeuralNetwork:
 		full_predicted_values=None
 		for i in reversed(range(len(data_to_eval))):
 			data=data_to_eval[i]
-			to_append=self.data.dataset.reshapeLabelsFromNeuralNetwork(data['predicted'])
+			to_append=self.data.dataset.reshapeLabelsFromNeuralNetwork(data['predicted'].copy())
+			to_append=self.data.dataset.denormalizeLabelsFromNeuralNetwork(to_append)
 			if full_predicted_values is None:
 				full_predicted_values=to_append
 			else:
@@ -114,10 +115,10 @@ class NeuralNetwork:
 		dates=[dt.datetime.strptime(d,"%d/%m/%Y").date() for d in self.data.dataset.getIndexes()]
 
 		# process predictions
-		first_value_predictions=[real_values[i][:self.hyperparameters.backwards_samples-1] for i in range(self.hyperparameters.amount_companies)]
-		last_value_predictions=[real_values[i][:self.hyperparameters.backwards_samples-1] for i in range(self.hyperparameters.amount_companies)]
-		mean_value_predictions=[real_values[i][:self.hyperparameters.backwards_samples-1] for i in range(self.hyperparameters.amount_companies)]
-		fl_mean_value_predictions=[real_values[i][:self.hyperparameters.backwards_samples-1] for i in range(self.hyperparameters.amount_companies)]
+		first_value_predictions=[[] for i in range(self.hyperparameters.amount_companies)]
+		last_value_predictions=[[] for i in range(self.hyperparameters.amount_companies)]
+		mean_value_predictions=[[] for i in range(self.hyperparameters.amount_companies)]
+		fl_mean_value_predictions=[[] for i in range(self.hyperparameters.amount_companies)]
 		for pred_val in full_predicted_values:
 			pred_val=np.swapaxes(pred_val,0,1)
 			for i in range(self.hyperparameters.amount_companies):
@@ -149,8 +150,9 @@ class NeuralNetwork:
 		model_metrics=aux
 		metrics={'Model Metrics':model_metrics,'Strategy Metrics':[],'Class Metrics':[]}
 		for i in range(self.hyperparameters.amount_companies):
-			swing_return,buy_hold_return,class_metrics_tmp=Actuator.analyzeStrategiesAndClassMetrics(real_values[i],fl_mean_value_predictions[i])
-			viniccius13_return=Actuator.autoBuy13(real_values[i],fl_mean_value_predictions[i])
+			real_value_without_backwards=real_values[i][self.hyperparameters.backwards_samples-1:]
+			swing_return,buy_hold_return,class_metrics_tmp=Actuator.analyzeStrategiesAndClassMetrics(real_value_without_backwards,fl_mean_value_predictions[i])
+			viniccius13_return=Actuator.autoBuy13(real_value_without_backwards,fl_mean_value_predictions[i])
 			strategy_metrics={}
 			class_metrics={}
 			if self.hyperparameters.amount_companies>1:
@@ -181,10 +183,10 @@ class NeuralNetwork:
 		if plot:
 			for i in range(self.hyperparameters.amount_companies):
 				plt.plot(dates,real_values[i], label='Real')
-				plt.plot(dates,real_values[i], color='r', label='Predicted F')
-				plt.plot(dates,real_values[i], color='g', label='Predicted L')
-				plt.plot(dates,real_values[i], color='c', label='Predicted Mean')
-				plt.plot(dates,real_values[i], color='k', label='Predicted FL Mean')
+				plt.plot(dates[self.hyperparameters.backwards_samples-1:],first_value_predictions[i], color='r', label='Predicted F')
+				plt.plot(dates[self.hyperparameters.backwards_samples-1:],last_value_predictions[i], color='g', label='Predicted L')
+				plt.plot(dates[self.hyperparameters.backwards_samples-1:],mean_value_predictions[i], color='c', label='Predicted Mean')
+				plt.plot(dates[self.hyperparameters.backwards_samples-1:],fl_mean_value_predictions[i], color='k', label='Predicted FL Mean')
 				if self.hyperparameters.amount_companies>1:
 					plt.title('Stock values {} | Company {} of {}'.format(self.data.dataset.name,(i+1),self.hyperparameters.amount_companies))
 				else: